7f0a710c0ff09952248fa25eae5487963f755bb2
NJ Development <nj.development83@gmail.com>
2023-09-14 14:44:46 -0300

    Initial Commit

---
+++ WhereIsIt/WhereIsItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereIsItApp.swift
+//  WhereIsIt
+//
+//  Created by NJ Development on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WhereIsItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
